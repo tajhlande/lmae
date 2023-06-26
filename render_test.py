1bd4fb2f06a2689581fb0c03afafe2fc5ec34a9e
Tajh Taylor <tajhlande@gmail.com>
2023-06-26 15:46:39 -0400

    Repositioning Kirby to better show grass alpha composition

--- render_test.py
+++ render_test.py
@@ -10,7 +10,7 @@ options = parse_matrix_options_command_line()
 matrix = RGBMatrix(options=options)
 stage = Stage(matrix=matrix, matrix_options=options)
 
-kirby = StillImage(name='Kirby', position=(20, 5), image=Image.open("images/kirby_22.png").convert('RGBA'))
+kirby = StillImage(name='Kirby', position=(20, 12), image=Image.open("images/kirby_22.png").convert('RGBA'))
 trees = StillImage(name='Trees', image=Image.open("images/trees-composite.png").convert('RGBA'))
 grass = StillImage(name='Grass', image=Image.open("images/grass.png").convert('RGBA'))
 stage.actors.extend((trees, kirby, grass))
