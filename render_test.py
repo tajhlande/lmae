6c87e716d5da0a6d11d587cd9add530e3a22024d
Tajh Taylor <tajhlande@gmail.com>
2023-07-05 13:07:13 -0400

    Adding Gorgeous Pixel font

--- render_test.py
+++ render_test.py
@@ -28,12 +28,13 @@ grass = StillImage(name='Grass', image=Image.open("images/grass.png").convert('R
 words = Text(name='Text', text="Hello, world!", position=(5, 5),
              # the following are the fonts native point sizes to avoid blurring
              # font=ImageFont.truetype("fonts/et-bt6001-font/EtBt6001-JO47.ttf", 12),
+             font=ImageFont.truetype("fonts/gorgeous-font/GorgeousPixel-BWO85.ttf", 5),
              # font=ImageFont.truetype("fonts/hardpixel-font/Hardpixel-nn51.otf", 14), # not a perfect fit
              # font=ImageFont.truetype("fonts/oseemono-font/Oseemono-V5Ez.ttf", 16),
              # font=ImageFont.truetype("fonts/poh-pixels-font/PohPixelsRegular-ljXw.ttf", 16),
              # font=ImageFont.truetype("fonts/sd-another-dimension-font/SdAnotherDimension-ljed.ttf", 9),
              # font=ImageFont.truetype("fonts/sparkly-font/SparklyFontRegular-zyA3.ttf", 8),  # placement is top line
-             font=ImageFont.truetype("fonts/teeny-tiny-pixls-font/TeenyTinyPixls-o2zo.ttf", 5),
+             # font=ImageFont.truetype("fonts/teeny-tiny-pixls-font/TeenyTinyPixls-o2zo.ttf", 5),
              color=(255, 255, 255, 255), stroke_color=(0, 0, 0, 255), stroke_width=1)
 stage.actors.extend((trees, words, moving_kirby, grass))
 
