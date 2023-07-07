1aa4ec349e28b492880519ce40882efabd3e793e
Tajh Taylor <tajhlande@gmail.com>
2023-07-06 22:02:22 -0400

    app module test

---
+++ lmae_module.py
@@ -0,0 +1,102 @@
+import time
+from abc import ABCMeta, abstractmethod
+from lmae_core import Stage
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+import logging
+
+
+class AppModule(metaclass=ABCMeta):
+    """
+    An app module is a self-contained app that renders itself on the LED matrix.
+    Apps that want to run should extend this class and implement the abstract methods.
+    An initialized matrix object and matrix options are provided for rendering purposes.
+    """
+    def __init__(self):
+        self.matrix = None
+        self.matrix_options = None
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.setLevel(logging.DEBUG)
+
+    def set_matrix(self, matrix: RGBMatrix, options: RGBMatrixOptions):
+        """
+        Called to set the matrix object. Must be called before any abstract methods are called.
+        :return:
+        """
+        self.logger.debug("Set matrix in app")
+        self.matrix = matrix
+        self.matrix_options = options
+
+    @abstractmethod
+    def prepare(self):
+        """
+        Apps can implement this method to prepare themselves before rendering.
+        :return: True if the method was successful, False if not (or throw an exception)
+        """
+        pass
+
+    @abstractmethod
+    async def run(self):
+        """
+        This method will be invoked async when the app should start running
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def stop(self):
+        """
+        Apps should stop running when this method is called.
+        :return:
+        """
+        pass
+
+
+class SingleStageRenderLoopAppModule(AppModule):
+    """
+    Using a single stage, render a loop of items
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.stage = Stage()
+        self.running = False
+
+    def add_actors(self, *args):
+        self.stage.actors.extend(args)
+
+    def prepare(self):
+        pass
+
+    async def run(self):
+        self.logger("Run started")
+        self.running = True
+        max_frame_rate = 120
+        min_time_per_frame = 1.0 / max_frame_rate
+        i = 0
+        last_time = time.time()
+        try:
+            while self.running:
+                # render the frame
+                self.stage.render_frame(i)
+                i += 1
+
+                # calculate the frame rate and render that
+                render_end_time = time.time()
+
+                # if we are rendering faster than max frame rate, slow down
+                elapsed_render_time = render_end_time - last_time
+                if elapsed_render_time < min_time_per_frame:
+                    # while (time.time() - last_time) < min_time_per_frame:
+                    #     pass
+                    time.sleep(min_time_per_frame - elapsed_render_time)
+
+                # mark the timestamp
+                last_time = time.time()
+        finally:
+            self.logger("Run stopped")
+
+    def stop(self):
+        self.logger("Got command to stop")
+        self.running = False
+
+
---
+++ lmae_module_test.py
@@ -0,0 +1,61 @@
+from lmae_core import parse_matrix_options_command_line
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+from lmae_module import AppModule, SingleStageRenderLoopAppModule
+
+import asyncio
+import collections
+import logging
+import time
+
+from pilmoji.source import AppleEmojiSource
+
+from lmae_core import Stage, StillImage, MovingActor, Text, EmojiText, parse_matrix_options_command_line
+from PIL import Image, ImageFont
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+
+logging.basicConfig(level=logging.INFO, format='%(relativeCreated)9d %(name)10s [%(levelname)5s]: %(message)s')
+logger = logging.getLogger("app_module_test")
+logger.setLevel(logging.DEBUG)
+print("LED Matrix Module Test")
+
+options: RGBMatrixOptions = parse_matrix_options_command_line()
+matrix = RGBMatrix(options=options)
+
+
+# set up stage
+def kirby_movement(frame_number: int) -> tuple[int, int]:
+    return frame_number % 86 - 22, 12
+
+
+kirby = StillImage(name='Kirby', position=(20, 12), image=Image.open("images/kirby_22.png").convert('RGBA'))
+moving_kirby = MovingActor(kirby, name="Moving Kirby", movement_function=kirby_movement)
+trees = StillImage(name='Trees', image=Image.open("images/trees-composite.png").convert('RGBA'))
+grass = StillImage(name='Grass', image=Image.open("images/grass.png").convert('RGBA'))
+words = Text(name='Text', text="Hello,\nworld!", position=(5, 5),
+             font=ImageFont.truetype("fonts/et-bt6001-font/EtBt6001-JO47.ttf", 6),  # good option for fitting a lot
+             color=(255, 255, 255, 255), stroke_color=(0, 0, 0, 255), stroke_width=1)
+
+app = SingleStageRenderLoopAppModule()
+app.set_matrix(matrix, options=options)
+app.add_actors(trees, words, moving_kirby, grass)
+
+
+async def stop_app(app: AppModule):
+    logger.debug("Stop app")
+    input("Press any key to stop app")
+    app.stop()
+
+
+async def run_app(app: AppModule):
+    logger.debug("run_app() called")
+    app_run_task = asyncio.create_task(app.run())
+    app_stop_task = asyncio.create_task(stop_app(app))
+    logger.debug("Awaiting app run task")
+    await app_run_task
+    logger.debug("Awaiting app stop task")
+    await app_stop_task
+    logger.debug("run_app() finished")
+
+
+run_app(app)
+
