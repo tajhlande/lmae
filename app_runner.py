e806eb037af678edce3e5c0047f32edd28d077ba
tajhlande <tajhlande@users.noreply.github.com>
2023-08-18 22:12:16 -0400

    Adding weather app

---
+++ app_runner.py
@@ -0,0 +1,53 @@
+from lmae_module import AppModule
+from threading import Thread
+import logging
+import os
+import configparser
+from lmae_core import parse_matrix_options_command_line
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+from weather_app import WeatherApp
+
+logging.basicConfig(level=logging.INFO, format='%(relativeCreated)9d %(name)10s [%(levelname)5s]: %(message)s')
+logger = logging.getLogger("app_runner")
+logger.setLevel(logging.DEBUG)
+print("App Runner")
+
+options: RGBMatrixOptions = parse_matrix_options_command_line()
+logger.info("Initializing matrix")
+matrix = RGBMatrix(options=options)
+
+
+def stop_app(app: AppModule):
+    logger.info("***** Press return to stop the app *****")
+    input()
+    logger.debug("Return pressed")
+    app.stop()
+
+
+def run_app(app: AppModule):
+    logger.debug("run_app() called")
+    app.prepare()
+
+    logger.info("Starting stopper thread")
+    stopper_thread = Thread(target=stop_app, args=[app])
+    stopper_thread.start()
+
+    logger.info("Running app")
+    app_thread = Thread(target=app.run)
+    app_thread.start()
+
+    stopper_thread.join()
+    app_thread.join()
+
+    logger.debug("run_app() finished")
+
+
+api_key = os.environ.get('VX_API_KEY')
+
+if not api_key:
+    config = configparser.ConfigParser()
+    config.read('env.ini')
+    api_key = config['visual.crossing']['vx_api_key']
+
+wx_app = WeatherApp(zipcode='20895', api_key=api_key)
+run_app(wx_app)
---
+++ weather_app.py
@@ -0,0 +1,86 @@
+from lmae_module import AppModule
+import time
+from threading import Lock
+from vx_wx.vx_client import get_current_conditions_by_zipcode
+from lmae_core import Stage, Text
+from PIL import ImageFont
+
+
+class WeatherApp(AppModule):
+    """
+    Display the current weather
+    """
+
+    def __init__(self, api_key: str, zipcode: str):
+        super().__init__()
+        self.temperature_label = None
+        self.api_key = api_key
+        self.zipcode = zipcode
+        self.logger.info(f"Checking weather for ZIP code {self.zipcode}")
+        self.running = False
+        self.lock = Lock()
+        self.stage = None
+        self.actors = list()
+        self.pre_render_callback = None
+        self.current_conditions = None
+        self.call_status = "ok"
+        self.temperature_font = ImageFont.truetype("fonts/press-start-2p-font/PressStart2P-vaV7.ttf", 8)
+
+    def get_current_conditions(self):
+        try:
+            self.logger.debug(f"Fetching current conditions for ZIP code {self.zipcode}")
+            current_conditions = get_current_conditions_by_zipcode(self.zipcode, self.api_key)
+            if current_conditions:
+                self.logger.debug("Call to get conditions succeeded")
+                self.current_conditions = current_conditions
+                self.call_status = "ok"
+            else:
+                self.logger.error("Call to get conditions did not return any conditions")
+                self.call_status = "error"
+                # old conditions remain available
+        finally:
+            self.logger.error("Call to get weather status failed")
+
+    def compose_view(self):
+        self.stage = Stage(matrix=self.matrix, matrix_options=self.matrix_options)
+        self.temperature_label = Text(name='temperature', position=(5, 5), font=self.temperature_font,
+                                      color=(255, 255, 255, 255), stroke_color=(0, 0, 0, 255), stroke_width=1)
+        self.stage.actors.append(self.temperature_label)
+
+    def update_view(self):
+        temperature = round(self.current_conditions['currentConditions']['temp'])
+        self.temperature_label.text = str(temperature)
+
+    def prepare(self):
+        self.compose_view()
+
+    def run(self):
+        self.logger.debug("Run started")
+        self.running = True
+        self.compose_view()
+
+        try:
+            while self.running:
+                # get updated weather conditions
+                self.get_current_conditions()
+
+                # update the view
+                self.update_view()
+                self.stage.render_frame(1)    # frame numbers don't matter here
+
+                # wait 15 minutes
+                waiting = True
+                wait_start = time.time()
+                while waiting:
+                    time.sleep(1)
+                    current_time = time.time()
+                    waiting = (current_time - wait_start) < (15 * 60)
+
+        finally:
+            self.logger.debug("Run stopped")
+
+    def stop(self):
+        self.logger.debug("Got command to stop")
+        self.running = False
+
+
