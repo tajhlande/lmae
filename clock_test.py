dca6233e07e27a2b8549d8dee3293c558ebcfdd3
Tajh Taylor <tajhlande@gmail.com>
2023-08-21 17:55:10 -0400

    clock test adjustments

--- clock_test.py
+++ clock_test.py
@@ -3,10 +3,11 @@ from functools import reduce
 
 
 def measure_clock():
-    t0 = time.clock()
-    t1 = time.clock()
+    t0 = time.perf_counter()
+    t1 = time.perf_counter()
     while t1 == t0:
-        t1 = time.clock()
+        t1 = time.perf_counter()
     return t0, t1, t1-t0
 
-reduce(lambda a,b: a+b, [measure_clock()[2] for i in range(1000000)] )/1000000.0
+
+reduce(lambda a, b: a+b, [measure_clock()[2] for i in range(1000000)])/1000000.0
