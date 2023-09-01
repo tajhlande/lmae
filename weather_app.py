39949cc28ca5242053af6beee46b9a2443c223c8
Tajh Taylor <tajhlande@gmail.com>
2023-08-31 23:07:42 -0400

    testing other temp display

--- weather_app.py
+++ weather_app.py
@@ -108,13 +108,13 @@ class WeatherApp(AppModule):
         self.temperature_label.text = temperature
 
         # dewpoint
-        dewpoint = f"DP {round(self.current_conditions['currentConditions']['dew'])}º"
+        dewpoint = f"Dew {round(self.current_conditions['currentConditions']['dew'])}º"
         # self.logger.debug(f"    Dewpoint: {dewpoint}")
         self.dewpoint_label.text = dewpoint
         self.dewpoint_label.set_visible(False)
 
         # feels like
-        feels_like = f"FL {round(self.current_conditions['currentConditions']['feelslike'])}"
+        feels_like = f"Feel {round(self.current_conditions['currentConditions']['feelslike'])}"
         # self.logger.debug(f"    Feels like: {feels_like}")
         self.feels_like_label.text = feels_like
         self.feels_like_label.set_visible(False)
@@ -123,13 +123,13 @@ class WeatherApp(AppModule):
         low_temp = f"Low {round(self.current_conditions['days'][0]['tempmin'])}"
         # self.logger.debug(f"    Forecast low temperature: {low_temp}")
         self.low_temp_label.text = low_temp
-        self.low_temp_label.set_visible(True)
+        self.low_temp_label.set_visible(False)
 
         # high temp
-        high_temp = f"Low {round(self.current_conditions['days'][0]['tempmax'])}"
+        high_temp = f"Hi {round(self.current_conditions['days'][0]['tempmax'])}"
         # self.logger.debug(f"    Forecast high temperature: {high_temp}")
         self.high_temp_label.text = high_temp
-        self.high_temp_label.set_visible(False)
+        self.high_temp_label.set_visible(True)
 
         # figure out whether it is day or night
         time_of_day = time.strftime("%H:%M:%S", time.localtime())
