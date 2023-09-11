c734ffb62163588da68561c21cd17d5046e466b2
Tajh Taylor <tajhlande@gmail.com>
2023-09-11 13:30:53 -0400

    Removing location information from code

--- openweather/openweather_client_test.py
+++ openweather/openweather_client_test.py
@@ -4,16 +4,24 @@ import json
 import os
 from openweather_client import get_conditions_and_forecast_by_lat_long
 
+config = configparser.ConfigParser()
+config.read('env.ini')
 
 api_key = os.environ.get('OW_API_KEY')
 
 if not api_key:
-    config = configparser.ConfigParser()
-    config.read('env.ini')
     api_key = config['openweather']['ow_api_key']
 
 logging.info(f"Got api key: {'true' if api_key else 'false'}")
 
-wj = get_conditions_and_forecast_by_lat_long('39.0158678', '-77.0734945', api_key)
+latitude = os.environ.get('LATITUDE')
+if not latitude:
+    latitude = config['location']['latitude']
+
+longitude = os.environ.get('LONGITUDE')
+if not longitude:
+    longitude = config['location']['longitude']
+
+wj = get_conditions_and_forecast_by_lat_long(latitude=latitude, longitude=longitude, api_key=api_key)
 
 print(json.dumps(wj, sort_keys=True, indent=4))
