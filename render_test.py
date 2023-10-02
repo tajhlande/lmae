6973ab3d25fdfa63535580d18936e7d8bafbeafb
Tajh Taylor <tajhlande@gmail.com>
2023-10-02 00:17:18 -0400

    Fixing render_test.py

--- render_test.py
+++ render_test.py
@@ -1,33 +1,20 @@
-import collections
 import logging
-import time
 
 from pilmoji.source import AppleEmojiSource
-
-from lmae_core import Stage, parse_matrix_options_command_line
-from lmae_actor import StillImage, MovingActor, Text, EmojiText
 from PIL import Image, ImageFont
-from rgbmatrix import RGBMatrix, RGBMatrixOptions
+
+from lmae_actor import StillImage, Text, EmojiText
+from lmae_animation import StraightMove
+from lmae_module import SingleStageRenderLoopAppModule
+import app_runner
 
 logging.basicConfig(level=logging.INFO, format='%(relativeCreated)9d %(name)10s [%(levelname)5s]: %(message)s')
 logger = logging.getLogger("render_test")
 logger.setLevel(logging.DEBUG)
 print("LED Matrix Rendering Test")
 
-# stdscr = curses.initscr()
-
-options: RGBMatrixOptions = parse_matrix_options_command_line()
-
-matrix = RGBMatrix(options=options)
-stage = Stage(matrix=matrix, matrix_options=options)
-
-
-def kirby_movement(frame_number: int) -> tuple[int, int]:
-    return frame_number % 86 - 22, 12
-
-
-kirby = StillImage(name='Kirby', position=(20, 12), image=Image.open("images/kirby_22.png").convert('RGBA'))
-moving_kirby = MovingActor(kirby, name="Moving Kirby", movement_function=kirby_movement)
+kirby = StillImage(name='Kirby', position=(-22, 12), image=Image.open("images/kirby_22.png").convert('RGBA'))
+kirby_movement = StraightMove(name="Moving Kirby", actor=kirby, duration=2.0, distance=(86, 0), repeat=True)
 trees = StillImage(name='Trees', image=Image.open("images/trees-composite.png").convert('RGBA'))
 grass = StillImage(name='Grass', image=Image.open("images/grass.png").convert('RGBA'))
 words = Text(name='Text', text="Hello,\nworld!", position=(5, 5),
@@ -47,50 +34,13 @@ emoji_words = EmojiText(name='EmojiText', text="☀️🌤️⛈️🌗", positi
                         emoji_source=AppleEmojiSource,
                         color=(255, 255, 255, 255),
                         stroke_color=(0, 0, 0, 255), stroke_width=1)
-stage.actors.extend((trees, emoji_words, moving_kirby, grass))
 
 
-print("Press CTRL-C to stop render loop")
-i = 0
-previous_elapsed_times = collections.deque((), 100)
-max_frame_rate = 120
-min_time_per_frame = 1.0 / max_frame_rate
-try:
-    # curses_window = curses.newwin(1, 7)
-    # hz_pos = curses_window.getyx()
-    last_time = time.time()
-    while True:
-        # render the frame
-        stage.render_frame(i)
-        i = i + 1
+app_runner.app_setup()
+sample_app = SingleStageRenderLoopAppModule()
+sample_app.set_matrix(app_runner.matrix, options=app_runner.matrix_options)
+sample_app.add_actors(trees, emoji_words, kirby, grass)
+sample_app.add_animations(kirby_movement)
 
-        # calculate the frame rate and render that
-        average_time = 1 if len(previous_elapsed_times) == 0 \
-            else sum(previous_elapsed_times) / len(previous_elapsed_times)
-        # curses_window.erase()
-        # curses_window.addstr(hz_pos[0], hz_pos[1], f"{round(1.0 / average_time)} Hz")
-        # curses_window.refresh()
-        render_end_time = time.time()
-
-        # if we are rendering faster than max frame rate, slow down
-        elapsed_render_time = render_end_time - last_time
-        if elapsed_render_time < min_time_per_frame:
-            # while (time.time() - last_time) < min_time_per_frame:
-            #     pass
-            time.sleep(min_time_per_frame - elapsed_render_time)
-
-        # record total elapsed time
-        end_time = time.time()
-        elapsed_time = end_time - last_time
-        previous_elapsed_times.appendleft(elapsed_time)
-        last_time = end_time
-
-except KeyboardInterrupt as ki:
-    print('\n')
-    logging.info("User pressed CTRL-C")
-
-finally:
-    pass
-    # curses.endwin()
-
+app_runner.start_app(sample_app)
 
