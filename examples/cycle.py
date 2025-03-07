e39e559e7c0d1b63df2ed009cce872d98e2ed9f8
Tajh Taylor <tajhlande@gmail.com>
2025-03-07 16:27:32 -0500

    adding sudo

--- examples/cycle.py
+++ examples/cycle.py
@@ -51,8 +51,9 @@ async def run_app_as_subprocess_with_timeout(python_path: str, app_script_path: 
 
     try:
         log.debug(f"Executing app subprocess to run for {timeout} seconds")
+        log.debug(f"exec: {python_path} {app_script_path}")
 
-        process = subprocess.run(args=[python_path, app_script_path], check=True, shell=True,
+        process = subprocess.run(args=["sudo", python_path, app_script_path], check=True, shell=True,
                                  stdout=sys.stdout, stderr=sys.stderr)
         try:
             outs, errs = process.communicate(timeout=timeout)
